dc82886a4f89f186244ca84e83b8a6885e3f4bb2
Martin Hansen <molheh@nuuday.dk>
2020-10-27 10:43:07 +0100

    fixed batch function for facebook

--- src/facebook.py
+++ src/facebook.py
@@ -1,9 +1,14 @@
 import requests
 import urllib
+import jmespath
+import numpy as np
+import json
+
+from utils import utils
 
 APP_ID = '2829655310472691'
 APP_SECRET = '9927ccb2fd615ac4ece7165b1e6e7e8c'
-ACCESS_TOKEN = 'EAAoNjneeFfMBAIwzZAvZAX5zgUh0BO1zs6bBm85xcMFdeDwSfsMhYXiIBhwC65AAZBvZAstfmBgUjlGUk7MhyHgZBroLuZBCm3QUP5ByoPZAHJjl13HJaO9aZB8RFjmTZAAfAaej8eBf4HZB0edZBLSzXf9sg6dC6v3oV7dviTeMO6ZA0gZDZD'
+ACCESS_TOKEN = 'EAAoNjneeFfMBAFtaKzEyXZC1s5ib4ssCfIFne1GhpubIfT3tzvjGzZB5toKaDgU7QNPcC8TLGqaxEBgTU36tx7YrfgPs50u4VAni8AlmnTHzc5DT9C6GQqFes9ZBV5sbgVP2nunIIxnG7nNGkyoqLRUW1WblNwZD'
 
 
 class FacebookAccount:
@@ -19,24 +24,25 @@ class FacebookAccount:
 
         if fields:
             params.update({'fields': ','.join(fields)})
-
-        if request_type == 'GET':
-            res = requests.get(f'{self.base_url}/{self.api_version}/{edge}',
-                params=params).json()
-            if not res['data'] and 'after' in params:
-                print('No further results')
-                return res['data'], None
-            return res['data'], res['paging']['cursors']
-        elif request_type == 'POST':
-            res = requests.post(f'{self.base_url}/{self.api_version}/{edge}',
-                params=params).json()
-            if not res['data'] and 'after' in params:
-                print('No further results')
-                return res['data'], None
-            return res['data'], res['paging']['cursors']
-        else:
-            raise NotImplementedError()
-
+        try:
+            if request_type == 'GET':
+                res = requests.get(f'{self.base_url}/{self.api_version}/{edge}',
+                    params=params).json()
+                if not res['data'] and 'after' in params:
+                    print('No further results')
+                    return res['data'], None
+                return res['data'], res['paging']['cursors']
+            elif request_type == 'POST':
+                res = requests.post(f'{self.base_url}/{self.api_version}/{edge}',
+                    params=params).json()
+                if not res['data'] and 'after' in params:
+                    print('No further results')
+                    return res['data'], None
+                return res['data'], res['paging']['cursors']
+            else:
+                raise NotImplementedError()
+        except:
+            print('token expired')
 
     def batch_request_facebook(self, edge: str, batch_lst: list, params: dict={}, fields: list=[], request_type='GET'):
         req_params = {'access_token': self.access_token}
@@ -49,9 +55,8 @@ class FacebookAccount:
             {'method': request_type,
             'relative_url': f'{self.api_version}/{batch_obj}/{edge}?{params}'} for batch_obj in batch_lst]
         req_params.update({'batch': batch})
-
         res = requests.post(self.base_url,
-            json=req_params).json()
+            json=req_params).text
         return res
         #if not res['data'] and 'after' in params:
         #    print('No further results')
@@ -65,11 +70,11 @@ class FacebookAccount:
         while paging:
             res, paging = self.request_facebook(edge=f'{account_id}/campaigns', params={'limit': '200', 'after': paging['after']})
             data.extend(res)
-        data = [dat['id'] for dat in data]
-        return data
+        campaign_ids = jmespath.search('[*].id', data)
+        return campaign_ids
 
-    def get_insights(self, campaign_lst: list):
-        res = self.batch_request_facebook(edge=f'insights', batch_lst=campaign_lst, fields=['impressions', 'spend', 'ad_id', 'adset_id'])
+    def get_insights(self, campaign: str):
+        res = self.request_facebook(edge=f'{campaign}/insights', params={'limit': '200'}, fields=['impressions', 'spend', 'ad_id', 'adset_id&level=ad'])
         return res
     
     def get_long_lived_access_token(self):
@@ -79,25 +84,38 @@ class FacebookAccount:
         res = self.request_facebook(edge='oauth/access_token', params=payload)
         return res['access_token'], res['expires_in']
 
-    def get_campaign_insights_batch():
-        raise NotImplementedError()
+    def get_campaign_insights_batch(self, campaign_lst: list):
+        campaign_lst_lst = utils.split_list(campaign_lst, 50)
+        res_lst = []
+        for campaign_batch in campaign_lst_lst:
+            batch_res = self.batch_request_facebook(edge=f'insights', batch_lst=campaign_batch, fields=['impressions', 'spend']) #, 'ad_id', 'adset_id&level=ad'
+            batch_res = batch_res.replace('"{', '{').replace('}"', '}').replace('\\"', '"').replace('""', '"')
+            batch_res = json.loads(batch_res)
+            res = jmespath.search('[].body[].data[*]. \
+                {impressions: impressions, spend: spend, date_start: date_start, date_stop: date_stop}', batch_res)
+            campaign_batch_dct = [{'campaign_id': campaign_id} for campaign_id in campaign_batch]
+            res = utils.standardize_lst_dct(res)
+            res = utils.dict_zip(res, campaign_batch_dct)
+            res_lst.extend(res)
+        df_insights = pd.DataFrame.from_dict(res_lst)
+        return df_insights
 
 
 def main():
     account = FacebookAccount(APP_ID, APP_SECRET, ACCESS_TOKEN)
     campaigns = account.get_campaigns()
-    res = account.get_insights(campaigns[0:1])
+    #res = account.get_insights(campaigns[1])
+    df_insights = account.get_campaign_insights_batch(campaigns)
 
 if __name__ == '__main__':
     main()
 
-campaigns
 
 
 # note to self
 # make the whole app, and then get standard-access or a system user to get an access token without expiry
 
-# Btach
+# Batch
 curl \
 -F 'access_token=<ACCESS_TOKEN>' \
 -F 'batch=[ \
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,28 @@
 import pandas as pd
+import numpy as np
 
 
 def pd_to_translate_dict(df: pd.DataFrame, col_from: str, col_to: str):
     translate_dct = dict(zip(df[col_from], df[col_to]))
     return translate_dct
+
+def split_list(lst: list, chunk_size: int):
+    return [lst[offs:offs+chunk_size] for offs in range(0, len(lst), chunk_size)]
+
+def standardize_lst_dct(lst: list):
+    try:
+        keys = set([k for dct in lst for k,v in dct.items()])
+        std_lst = [dct if dct else {k: np.nan for k in keys} for dct in lst]
+    except AttributeError:
+        keys = set([k for dct in lst if dct for k,v in dct[0].items()])
+        std_lst = [dct[0] if dct else {k: np.nan for k in keys} for dct in lst]     
+    return std_lst
+
+def dict_zip(lst1: list, lst2: list):
+    """ 
+    zip together two lists of equal size that contains dicts to one list of dicts
+    """
+    assert len(lst1) == len(lst2), 'The list of dicts should be equally sized'
+    [lst1[i].update(lst2[i]) for i in range(0, len(lst1))]
+    # returning lst1 because of the update method
+    return lst1
