891f660d5e7e4dd9a4d2d698f7b0da7a22352070
Martin Hansen <molheh@nuuday.dk>
2020-11-18 10:30:50 +0100

    added view statement

--- src/main.py
+++ src/main.py
@@ -2,8 +2,14 @@ import google_ads
 import google_campaign_manager
 import facebook
 
+import settings
+from utils import sql_utils
+import sql
 
 if __name__ == "__main__":
     google_ads.main()
     google_campaign_manager.main()
     facebook.main()
+
+    mariadb_engine = sql_utils.create_engine(settings.MARIADB_CONFIG, db_name='output', db_type='mysql')
+    mariadb_engine.execute(sql.COLLECTED_VIEW)
